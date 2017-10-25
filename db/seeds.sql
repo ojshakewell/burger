17085b64f1e2c314a98da8b6e53ece9d796c7452
Brian Russell <brianrobertrussell@gmail.com>
2017-10-24 22:28:26 -0600

    orm folders setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured) 
+VALUES ('Royale with Cheese',FALSE);
+INSERT INTO burgers(burger_name, devoured) 
+VALUES ('Harold and Kumar Slider',FALSE);
+INSERT INTO burgers(burger_name, devoured) 
+VALUES ('McDowell\'s Big Mick',TRUE);
